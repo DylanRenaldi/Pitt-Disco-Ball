e7d6c6836602095b6a971423ae3dd173fb9d166f
ctc-git <105753703+ctc-git@users.noreply.github.com>
2025-07-28 19:07:14 -0400

    Update bluetooth_led.ino

--- test/bluetooth_led.ino
+++ test/bluetooth_led.ino
@@ -1,56 +1,108 @@
-// will blink the BLUETOOTH_LED light for 1 second if the system is either connected or disconnected
-// further logic will be implemented to indicate modes:
-// 3-second blink: your bluetooth device has connected
-// 3x 1-second blinks:  your bluetooth device has disconnected
-// 1 second blink:  bluetooth device has successfully updated mode
-// etc
+
+#include <BluetoothSerial.h>
+
+#include "../disco_peripherals.h"
+// #include "../disco_modules.h"
 
 
-
-// #include "../disco_peripherals.h"  -> #define BLUETOOTH_LED 27
-
-#define BLUETOOTH_LED 27
+BluetoothSerial ESP_BT;
 
 void setup() {
   Serial.begin(115200);
+  ESP_BT.begin("ESP-Controller");
   
+  pinMode(INCREASE_BRIGHTNESS, INPUT_PULLUP);
+  pinMode(DECREASE_BRIGHTNESS, INPUT_PULLUP);
+
   pinMode(BLUETOOTH_LED, OUTPUT);
 }
 
+uint8_t BRIGHTNESS = 5;
+
+
 void loop() {
+  
+  static uint8_t diff = 0;
+	static uint8_t mode = 7;
+	static unsigned long lastf = millis();
 
-  static int8_t connected = false;
-  static uint16_t BLUETOOTH_BUFFER = 0;
-  static int lastFrame = millis(), diff;
 
-  diff = millis() - lastFrame;
-  lastFrame = millis();
+  if (frameInterval(diff)) return;
+	checkBluetooth(ESP_BT, diff, mode);
 
-  if(BLUETOOTH_BUFFER) {
-    if(!digitalRead(BLUETOOTH_LED))
-      digitalWrite(BLUETOOTH_LED, HIGH);
+	if (millis() - lastf > 5000) {		// every 5 seconds, synchronously
+		lastf = millis();
+		mode = random(4, 10);						// between 4 and 9
+	}
 
-    if(BLUETOOTH_BUFFER > diff)
-      BLUETOOTH_BUFFER -= diff;
-    else {
-      BLUETOOTH_BUFFER = 0;
-      digitalWrite(BLUETOOTH_LED, LOW);
-    }
-  } 
-
-  if (isClient(millis())) {
-    if (!connected) {
-      connected = true;
-      BLUETOOTH_BUFFER = 1000;      // 1000ms
-    }
-
-  } else if (connected) {
-    connected = false;
-    BLUETOOTH_BUFFER = 1000;        // 1000ms
-  }
 }
 
-// generic isClient function to resemble BluetoothSerial isClient function
-bool isClient(int milis){
-  return (milis/5000)&1;    // every 5000 milliseconds
+
+bool frameInterval(uint8_t &diff) {
+
+	static unsigned long lastFrame = millis();
+	static const uint8_t frameInterval = 25;          		// Target ~40 FPS
+
+	diff = millis() - lastFrame;            			// save diff because it may change in midst of below comparison and cause an erroneous error
+
+	if (diff < frameInterval) return true;
+	lastFrame = millis();
+	
+	return false;
+}
+
+void checkBluetooth(BluetoothSerial &ESP_BT, const uint8_t &diff, const uint8_t &mode) {
+
+	static bool connected = false;
+	static uint8_t iteration = 0,update = mode;                              // set to 0 and mode, respectively so nothing occurs first iteration
+	static uint16_t ON_BUFFER  = 0,
+									OFF_BUFFER = 0;
+
+	if (update != mode) {
+		OFF_BUFFER = 100 * (iteration > 0 || ON_BUFFER > 0);				// 100 ms if BLUETOOTH_LED is logically on in any facet
+		iteration  = update = mode;
+	}
+	
+	if (ESP_BT.hasClient() != connected) {
+		connected = !connected;
+		ON_BUFFER = 1000;																			// 1000 ms
+	}
+
+	if (ON_BUFFER) {	// ON_BUFFER takes priority
+		ON_BUFFER = (ON_BUFFER > diff ? ON_BUFFER - diff : 0);
+
+		if (ON_BUFFER != digitalRead(BLUETOOTH_LED))
+			digitalWrite(BLUETOOTH_LED, ON_BUFFER > 0);
+
+	} else if (OFF_BUFFER) {		// necessary between mode-update blink iterations
+		// if (digitalRead(BLUETOOTH_LED))
+		// 	digitalWrite(BLUETOOTH_LED, LOW);
+
+		OFF_BUFFER = (OFF_BUFFER > diff ? OFF_BUFFER - diff : 0);
+
+	} else if (iteration) {
+		ON_BUFFER = OFF_BUFFER = 100;
+		--iteration;
+	}
+}
+
+void debounceButtons(uint8_t &BRIGHTNESS, const uint8_t &diff) {
+
+	static uint8_t debounce = 0;
+
+	if(!debounce) {
+		uint8_t b = BRIGHTNESS;
+		if(digitalRead(INCREASE_BRIGHTNESS) && BRIGHTNESS < 100)    // GPIO17
+			++BRIGHTNESS;//FastLED.setBrightness(++BRIGHTNESS);
+
+		else if (digitalRead(DECREASE_BRIGHTNESS) && BRIGHTNESS)    // GPIO16
+			--BRIGHTNESS;//FastLED.setBrightness(--BRIGHTNESS);
+
+		if(b != BRIGHTNESS){
+			Serial.println(BRIGHTNESS);
+			debounce = 100;
+		}
+	} else {
+		debounce = (debounce >= diff ? debounce - diff : 0);
+	}
 }
