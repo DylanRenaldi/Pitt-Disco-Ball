98ed2a9a013ccb2257e67e90af9ca875fabcbee7
ctc-git <105753703+ctc-git@users.noreply.github.com>
2025-07-29 03:33:40 -0400

    Update namespace.ino

--- headers/namespace/namespace.ino
+++ headers/namespace/namespace.ino
@@ -2,25 +2,25 @@
 #include "disco.hpp"
 
 void setup() {
-  disco::init();
+	disco::init();
 }
 
 void loop() {
   
-  static uint8_t diff = 0,mode = 3;
-  if (disco::frameInterval(diff)) return;
-  disco::debounceButtons(diff);
-
-  static unsigned long lastf = millis();
-  if (millis() - lastf > 5000) {
-    lastf = millis();
-
-    mode = random(5, 9);
-    disco::checkBluetooth(diff, mode);
-  }
-
-  // modes
-  // etc.  
+	static uint8_t diff = 0,mode = 3;
+	if (disco::frameInterval(diff)) return;
+	disco::debounceButtons(diff);
+	
+	static unsigned long lastf = millis();
+	if (millis() - lastf > 5000) {
+		lastf = millis();
+		
+		mode = random(5, 9);
+		disco::checkBluetooth(diff, mode);
+	}
+	
+	// modes
+	// etc.  
 }
 
 // Convert (x, y) coordinate to 1D LED index based on matrix layout
