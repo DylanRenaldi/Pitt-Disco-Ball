bc03549ca67cdba8fcd57bbfd66c5a74049729ca
ctc-git <105753703+ctc-git@users.noreply.github.com>
2025-07-29 22:24:35 -0400

    Update disco_peripherals.h

--- library/disco_peripherals.h
+++ library/disco_peripherals.h
@@ -9,9 +9,9 @@
 #define NUM_LEDS          		(MATRIX_WIDTH * MATRIX_HEIGHT)
 
 // Peripherals
-#define INCREASE_BRIGHTNESS 	17			// increase brightness button -> GPIO17
-#define DECREASE_BRIGHTNESS 	16			// decrease brightness button -> GPIO16
-#define BLUETOOTH_LED       	27			// bluetooth LED indicator    -> GPIO27
+#define INCREASE_BRIGHTNESS 	        17			// increase brightness button -> GPIO17
+#define DECREASE_BRIGHTNESS 	        16			// decrease brightness button -> GPIO16
+#define BLUETOOTH_LED       	        27			// bluetooth LED indicator    -> GPIO27
 
 // I2S pin assignments
 #define I2S_WS            		25			// Word Select (LRCLK)
@@ -19,9 +19,13 @@
 #define I2S_SCK           		33			// Bit Clock (BCLK)
 
 // Audio sampling settings
-#define SAMPLES					512			// sample buffer size 	(bandwidth is 0 < SAMPLES/2)
-#define SAMPLING_FREQ			44100		// sample rate 			(bandwidth is SAMPLING_FREQ/2)
+#define SAMPLES				512			// sample buffer size 	(bandwidth is 0 < SAMPLES/2)
+#define SAMPLING_FREQ			44100		        // sample rate 		(bandwidth is SAMPLING_FREQ/2)
 
 // Visualization state variables
 #define peakHoldTime 			150			// Time in ms to hold peak before decay
 #define peakFallSpeed  			1			// Amount to reduce peak per update
+
+// Bluetooth Buffer Time Variables
+#define BT_CONNECTION			3000		        // 3000 ms
+#define BLINK_INTERVAL			100			// 100 ms, bluetooth led blink interval
